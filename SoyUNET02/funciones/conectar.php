27349266cc8a95b3066371b3ec4906ea2b72f7bb
chaconmariale <chaconmariale@gmail.com>
2015-04-08 18:05:57 -0430

    Revert "bad5d7d149e770de4b4a8c5b3892b4bf8729c766"
    This reverts commit 26e616202fae0fad90382818014e1afeb1dae262.

--- SoyUNET02/funciones/conectar.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function conectar($host, $user, $clav)
-	{
-		$link = mysql_connect('localhost', 'yrjsoyunet', '654321')
-	    or die('No se pudo conectar: ' . mysql_error());
-		mysql_select_db('soyunet') or die('No se pudo seleccionar la base de datos');
-		return $link;
-	}
-?>
--- SoyUNET02/includes/psl-config.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	define("HOST", "localhost");     // El alojamiento al que deseas conectarte
-	define("USER", "yrjsoyunet");    // El nombre de usuario de la base de datos
-	define("PASSWORD", "654321");    // La contraseña de la base de datos
-	define("DATABASE", "soyunet");    // El nombre de la base de datos
-	define("CAN_REGISTER", "any");
-	define("DEFAULT_ROLE", "m01_usa");
-	define("SECURE", FALSE);    // ¡¡¡SOLO PARA DESARROLLAR!!!!
-?>
